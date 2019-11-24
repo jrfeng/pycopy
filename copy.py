d3181f66eb4dbaedc2725f308356c8918daa0e33
jrfeng <jrfeng331@qq.com>
2019-11-24 10:38:29 +0800

    优化输出信息

--- copy.py
+++ copy.py
@@ -4,7 +4,7 @@ import os
 import shutil
 
 
-def read_config(config_file):
+def read_config(config_file: str):
     if not os.path.exists(config_file):
         print("[error]not find config file.")
         return
@@ -13,7 +13,7 @@ def read_config(config_file):
         return json.loads(config.read())
 
 
-def check_config(config):
+def check_config(config: dict):
     try:
         if config is None:
             return False
@@ -43,17 +43,35 @@ def check_config(config):
     return True
 
 
-def check_or_create_dir(parent_dir, child_dir):
+def check_or_create_dir(parent_dir: str, child_dir: str):
     if not os.path.exists(parent_dir + "/" + child_dir):
         print("create dir:", child_dir)
         os.mkdir(parent_dir + "/" + child_dir)
 
 
-def copy_all_files(input_dir, output_dir):
+def copy_all_files(input_dir: str, output_dir: str):
+    count = 0;
     for f in os.scandir(input_dir):
         if os.path.isfile(f):
-            print("    copy:", f.name)
+            print_copy_message(f.name, output_dir)
             shutil.copy(f, output_dir)
+            count += 1
+    print("    count:", count)
+
+
+def file_exists(parent_dir: str, file_name: str):
+    return os.path.exists(parent_dir + "/" + file_name)
+
+
+def print_copy_message(file_name: str, output_dir: str):
+    message = "    copy: " + file_name
+
+    if file_exists(output_dir, file_name):
+        message += " [override]"
+    else:
+        message += " [new]"
+
+    print(message)
 
 
 def main():
@@ -69,6 +87,9 @@ def main():
     input_dir = config["input_dir"]
     output_dir = config["output_dir"]
     dir_maps = config["dir_maps"]
+
+    print("input  dir:", input_dir)
+    print("output dir:", output_dir)
 
     for key, value in dir_maps.items():
         check_or_create_dir(output_dir, value)
